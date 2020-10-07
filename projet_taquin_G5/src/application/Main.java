12ebb89d2d268d85e4c37cadde18063109130c9b
tiphainewo <71008970+tiphainewo@users.noreply.github.com>
2020-10-07 11:13:36 +0200

    Ajouté chrono

--- projet_taquin_G5/src/application/Main.java
+++ projet_taquin_G5/src/application/Main.java
@@ -1,39 +1,43 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+
 import java.util.Scanner; 
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 
 
-public class Main /*extends Application*/ {
-	/*
-	@Override
+public class Main extends Application {
+	
 	public void start(Stage primaryStage) {
 		try {
+			Pane mainPane = (Pane) FXMLLoader.load(Main.class.getResource("Test.fxml"));
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			primaryStage.setScene(new Scene(mainPane));
 			primaryStage.show();
-			System.out.println("hello");
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-	*/
+	
 	
 	public static void main(String[] args) {
-		//launch(args);
+		launch(args);
+		Timer chrono = new Timer();
 		Grille grilleTest = new Grille(4);
 		grilleTest.afficherGrille();
 		Scanner sc = new Scanner(System.in); 
 		grilleTest.test();
-		while(true) {
+		//chrono.start();
+		/*while(true) {
 		
         char c = sc.next().charAt(0); 
         grilleTest.movePiece(c);
-		}
+		}*/
 	}
 }
