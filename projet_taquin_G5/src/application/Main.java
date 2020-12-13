ba14f91c2e46b61c514bae709f8421866ed2cffb
tiphainewo <71008970+tiphainewo@users.noreply.github.com>
2020-12-13 17:02:12 +0100

    Ajouté un titre à la fenêtre

--- projet_taquin_G5/src/application/Main.java
+++ projet_taquin_G5/src/application/Main.java
@@ -19,6 +19,7 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			primaryStage.setTitle("Jeu du Taquin - G5");
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
